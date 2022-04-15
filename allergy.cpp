15af8b2876baf89e177cd01d9f1c68b2f656c93d
wweidler <weidlerwill@gmail.com>
2022-04-14 20:45:11 -0500

    i cant spell

--- allergy.cpp
+++ allergy.cpp
@@ -12,11 +12,11 @@ void printSummary(Mapmaker player1, Mapmaker player2){
     cout << "\t\tMap:" << endl;
     player1.printMap();
     cout << "\t\tNumber of locations explored: " << player1.getExplored() << endl;
-    cout << "\t\tNumer of Rock Paper Scissors won: " << player1.getWins() << endl;
+    cout << "\t\tNumber of Rock Paper Scissors won: " << player1.getWins() << endl;
     cout << "\t" << player2.getName() << endl;
     cout << "\t\tMap:" << endl;
     player2.printMap();
     cout << "\t\tNumber of locations explored: " << player2.getExplored() << endl;
-    cout << "\t\tNumer of Rock Paper Scissors won: " << player2.getWins() << endl;
+    cout << "\t\tNumber of Rock Paper Scissors won: " << player2.getWins() << endl;
     return;
 }
